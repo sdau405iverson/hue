9afb0c573d29448dcb67240d51f6f55d986ba4a6
Ying Chen <yingchen@cloudera.com>
2018-01-10 12:14:59 -0800

    HUE-7860 [core] Integrate non IO blocking Python Webserver   Add runserver command

--- desktop/core/src/desktop/conf.py
+++ desktop/core/src/desktop/conf.py
@@ -128,6 +128,24 @@ def is_https_enabled():
   """Hue is configured for HTTPS."""
   return bool(SSL_CERTIFICATE.get() and SSL_PRIVATE_KEY.get())
 
+USE_CHERRYPY_SERVER = Config(
+  key="use_cherrypy_server",
+  help=_("If set to true, CherryPy will be used. Otherwise, Gunicorn will be used as the webserver."),
+  type=coerce_bool,
+  default=False)
+
+GUNICORN_WORKER_CLASS = Config(
+  key="gunicorn_work_class",
+  help=_("Gunicorn work class."),
+  type=str,
+  default="eventlet")
+
+GUNICORN_NUMBER_OF_WORKERS = Config(
+  key="gunicorn_number_of_workers",
+  help=_("Gunicorn number of works"),
+  type=int,
+  default=0)
+
 HTTP_HOST = Config(
   key="http_host",
   help=_("HTTP host to bind to."),
